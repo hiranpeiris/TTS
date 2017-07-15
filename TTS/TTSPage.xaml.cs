30181c73d128c121a9637829639703889b9a3857
Hiran Peiris <hiranpeiris.sl@gmail.com>
2017-07-15 10:08:17 +0530

    Init

---
+++ TTS/TTSPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Plugin.TextToSpeech;
+
+namespace TTS
+{
+    public partial class TTSPage : ContentPage
+    {
+        public TTSPage()
+        {
+            InitializeComponent();
+        }
+
+		private async void OnClick(object sender, EventArgs e)
+		{
+            var text = textEditor.Text.Trim();
+            speakButton.IsEnabled = false;
+
+            await CrossTextToSpeech.Current.Speak(text);
+
+            speakButton.IsEnabled = true;
+		}
+    }
+}
